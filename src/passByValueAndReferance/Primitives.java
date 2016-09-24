03af8537270d72c0d309923f03c5891091a8cdfd
Deepak <deepaksisupal@gmail.com>
2016-09-24 11:31:52 +0530

    update

--- src/passByValueAndReferance/Primitives.java
+++ src/passByValueAndReferance/Primitives.java
@@ -2,13 +2,31 @@ package passByValueAndReferance;
 
 public class Primitives {
 
-	int a = 10;
-	long l = 10L;
-	double d = 10D;
-	
 	public Primitives() {
-		
+
 	}
 
-	
+	public void trigger() {
+		int a = 10;
+		{
+			a = 11;
+			System.out.println("int a = " + a);
+
+			int modifiedA = modifyinteger(a);//pass by value since its a primitive
+
+			System.out.println("after modifyInterger() -> int a = " + a);
+			System.out.println("after modifyInterger() -> int modifiedA = " + modifiedA);
+		}
+		System.out.println("after {} scope -> int a = " + a);
+		// System.out.println("after modifyInterger() -> int modifiedA = " +
+		// modifiedA); //modifiedA cannot be resolved to a variable
+	}
+
+	private int modifyinteger(int a) {
+		System.out.println("Enter modifyInterger()");
+		a = 0;
+		System.out.println("modified int a = " + a);
+		System.out.println("Exiting  modifyInterger()");
+		return a;
+	}
 }
