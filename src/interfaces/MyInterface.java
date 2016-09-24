c8729c9ae9675388895167f44ff393f019e56378
Deepak <deepaksisupal@gmail.com>
2016-09-24 13:26:18 +0530

    update

---
+++ src/interfaces/MyInterface.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+// an interface is declared using the Java interface keyword.
+//declared public or package scope (no access modifier).
+public abstract interface MyInterface {
+	public String hello = "Hello";
+
+	public void sayHello();//by default, public and abstract
+}
